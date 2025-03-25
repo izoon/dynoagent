9e7b11b7fa65804b09a1cb651c59778dd277d5e7
V Rafizadeh <VSalami@gmail.com>
2025-03-25 08:47:23 -0400

    refactor: reorganize package structure to remove redundant directory and fix circular imports

--- dynoagent/__init__.py
+++ dynoagent/__init__.py
@@ -6,14 +6,13 @@ from .core import DynoAgent
 from .team import Team
 from .task_complexity import TaskComplexityAnalyzer
 from .dyno_agent_with_tools import DynoAgentWithTools
-from .cli import main
 
 __version__ = "0.1.0"
 __author__ = "izoon"
+
 __all__ = [
     "DynoAgent",
     "Team",
     "TaskComplexityAnalyzer",
     "DynoAgentWithTools",
-    "main"
 ] 
--- setup.py
+++ setup.py
@@ -33,8 +33,8 @@ setup(
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
-    package_dir={"": "dynoagent"},
-    packages=find_packages(where="dynoagent"),
+    package_dir={"": "."},
+    packages=find_packages(),
     python_requires=">=3.8",
     install_requires=[
         "numpy>=1.20.0",
